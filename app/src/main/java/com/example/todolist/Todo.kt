87c011a5f3743d242dfe1d100e05803f78f4253e
krwi20 <kristoffwittwer@icloud.com>
2022-09-25 20:51:55 +0100

    UI Setup

---
+++ app/src/main/java/com/example/todolist/Todo.kt
@@ -0,0 +1,6 @@
+package com.example.todolist
+
+class Todo (
+    val title: String,
+    val isChecked: Boolean = false
+        )
---
+++ app/src/main/java/com/example/todolist/TodoAdapter.kt
@@ -0,0 +1,5 @@
+package com.example.todolist
+
+class TodoAdapter {
+    private val
+}
